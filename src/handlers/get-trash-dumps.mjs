c2bad0f03a88fa193ac2356853533bed61b753ba
original-subbaman <abhisheksubba.py@gmail.com>
2025-12-30 09:59:23 +0530

    fix: remove uncesary functions and events feat/template: update template to create dynamodb tables and set env variables

---
+++ src/handlers/get-trash-dumps.mjs
@@ -0,0 +1,41 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
+
+const isLocal = !!process.env.DYNAMODB_ENDPOINT;
+console.log("🚀 ~ isLocal:", process.env.DYNAMODB_ENDPOINT);
+
+const client = new DynamoDBClient({
+  region: "ap-south-1",
+  endpoint: process.env.DYNAMODB_ENDPOINT,
+  credentials: isLocal
+    ? {
+        accessKeyId: "local",
+        secretAccessKey: "local",
+      }
+    : undefined,
+});
+
+const docClient = DynamoDBDocumentClient.from(client);
+
+export const getTrashDumpsHandler = async (event) => {
+  try {
+    console.log("MAIN_TABLE:", process.env.MAIN_TABLE);
+    console.log("DYNAMODB_ENDPOINT:", process.env.DYNAMODB_ENDPOINT);
+    const command = new ScanCommand({
+      TableName: process.env.MAIN_TABLE,
+    });
+
+    const data = await docClient.send(command);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data.Items ?? []),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: error.message }),
+    };
+  }
+};
