bc3e5bc5cef4acecb81725b9fa9ec58e48558a3c
original-subbaman <abhisheksubba.py@gmail.com>
2026-06-15 23:08:25 +0530

    feat: return photoKey instead of photoUrl - this is so that the photoKey can be used to generate temporary photo url from S3

--- src/handlers/storage/generate-upload-url.ts
+++ src/handlers/storage/generate-upload-url.ts
@@ -7,29 +7,34 @@ const s3 = new S3Client({ region: process.env.AWS_REGION });
 const BUCKET = process.env.DUMP_PINS_BUCKET;
 const URL_EXPIRATION_SECONDS = 300; // URL valid for 5 minutes
 const CONTENT_TYPE = "image/jpeg"; // Adjust as needed for different file types
-export async function generateUploadUrlHandler(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
-    try {
-        const userId = event.requestContext.authorizer?.claims?.sub;
-        const { fileType } = JSON.parse(event.body ?? "{}"); // e.g. "image/jpeg"
-        if (!fileType || !userId || fileType !== CONTENT_TYPE) {
-            return apiResponse(400, { message: "Invalid request" });
-        }
-        const key = `pins/${userId}/${crypto.randomUUID()}.jpg`;
-        const url = await getSignedUrl(s3, new PutObjectCommand({
-            Bucket: BUCKET,
-            Key: key,
-            ContentType: fileType,
-        }), { expiresIn: URL_EXPIRATION_SECONDS });
-        return apiResponse(200, {
-            uploadUrl: url, // frontend PUTs to this
-            photoUrl: `https://${BUCKET}.s3.amazonaws.com/${key}`, // save this in DynamoDB
-        });
+export async function generateUploadUrlHandler(
+  event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> {
+  try {
+    const userId = event.requestContext.authorizer?.claims?.sub;
+    const { fileType } = JSON.parse(event.body ?? "{}"); // e.g. "image/jpeg"
+    if (!fileType || !userId || fileType !== CONTENT_TYPE) {
+      return apiResponse(400, { message: "Invalid request" });
     }
-    catch (error) {
-        return apiResponse(500, {
-            message: "Failed to generate upload URL",
-            error: error instanceof Error ? error.message : String(error),
-        });
-    }
-}
+    const key = `pins/${userId}/${crypto.randomUUID()}.jpg`;
+    const url = await getSignedUrl(
+      s3,
+      new PutObjectCommand({
+        Bucket: BUCKET,
+        Key: key,
+        ContentType: fileType,
+      }),
+      { expiresIn: URL_EXPIRATION_SECONDS },
+    );
 
+    return apiResponse(200, {
+      uploadUrl: url, // frontend PUTs to this
+      photoKey: key, // save this in DynamoDB
+    });
+  } catch (error) {
+    return apiResponse(500, {
+      message: "Failed to generate upload URL",
+      error: error instanceof Error ? error.message : String(error),
+    });
+  }
+}
