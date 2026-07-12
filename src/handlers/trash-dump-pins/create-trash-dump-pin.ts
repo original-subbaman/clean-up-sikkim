c1743cbfb243ecb725121eca5e8b1f595cb7b40f
original-subbaman <abhisheksubba.py@gmail.com>
2026-07-12 16:00:35 +0530

    feat(create-trash-dump-pin): replace default pin status from "OPEN" to "REPORTED" - update pin statuses

--- src/handlers/trash-dump-pins/create-trash-dump-pin.ts
+++ src/handlers/trash-dump-pins/create-trash-dump-pin.ts
@@ -97,7 +97,7 @@ export const createTrashDumpPinHandler = async (
       geohash: geohash,
       geohash5: geohash.substring(0, 5),
       geohash4: geohash.substring(0, 4),
-      status: PIN_STATUS.OPEN,
+      status: PIN_STATUS.REPORTED,
       reportedBy: userId,
       reporterName: user.Item.name,
     };
